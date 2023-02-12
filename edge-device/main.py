f6ee1e4facc06f4d95e4eb2804988afae440c02f
Maresch Maximilian <maxi.maresch@gmail.com>
2023-02-12 12:30:34 +0100

    Implement argument parsing, add frame counter and requests setup

--- edge-device/main.py
+++ edge-device/main.py
@@ -1,16 +1,29 @@
+import argparse
 import cv2 as cv
+import requests
 
 
-def main():
-    capture = cv.VideoCapture(0)
+def main(path):
+    capture = cv.VideoCapture(path)
     if not capture.isOpened():
         print("Cannot open capture")
         exit()
+
+    frame_count = 0
+    detection_rate = 5
     while True:
         ret, frame = capture.read()
         if not ret:
             print("Cannot receive frame")
             break
+        frame_count += 1
+
+        if frame_count % detection_rate == 0:
+            encoded = cv.imencode(".jpg", frame)[1]
+            file = {'file': ('frame.jpg', encoded.tobytes(), 'image/jpeg')}
+            data = {"id": "1234"}
+            response = requests.post("http://127.0.0.1/detect/", files=file, data=data, timeout=5)
+            bounding_box = 0
 
         # Operations on the frame here
         cv.rectangle(frame, (20, 20), (500, 200), (255, 0, 0), 3)
@@ -23,4 +36,8 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path", nargs="?", default=0, help="path to video, defaults to using the camera")
+    args = parser.parse_args()
+
+    main(args.path)
