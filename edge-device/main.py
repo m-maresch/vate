6a84e036afed2ddabca69f829008898693418067
Maresch Maximilian <maxi.maresch@gmail.com>
2023-02-26 21:01:09 +0100

    Add fps, object tracking and refactor edge-device

--- edge-device/main.py
+++ edge-device/main.py
@@ -2,6 +2,9 @@ import argparse
 import cv2 as cv
 import requests
 import uuid
+import time
+
+font = cv.FONT_HERSHEY_SIMPLEX
 
 
 def main(path):
@@ -11,42 +14,103 @@ def main(path):
         exit()
 
     frame_count = 0
-    detection_rate = 5
-    displayed_detections = []
+    tracker_failures = 0
+    detection_rate = 6
+
+    detections_to_display = []
+    multi_tracker = None
+
+    prev_frame_at = 0
     while True:
         ret, frame = capture.read()
         if not ret:
             print("Cannot receive frame")
             break
-        frame_count += 1
 
         if frame_count % detection_rate == 0:
-            encoded = cv.imencode(".jpg", frame)[1]
-            frame_id = str(uuid.uuid4())
-            file = {'file': (f'{frame_id}.jpg', encoded.tobytes(), 'image/jpeg')}
-            data = {"id": f"{frame_id}"}
-            response = requests.post("http://127.0.0.1:8000/detection", files=file, data=data, timeout=5)
-            body = response.json()
-            print(f"Got: {body}")
-            displayed_detections = []
-            detections = body['detections']
+            detections_to_display = []
+            multi_tracker = _create_multi_tracker()
+            detections = _detect_objects(frame)
             for detection in detections:
-                bbox = detection['bbox']
-                x = int(bbox[0])
-                y = int(bbox[1])
-                w = int(bbox[2])
-                h = int(bbox[3])
-                displayed_detections.append((x, y, w, h))
+                (bbox, score, category) = _to_bbox_score_category(detection)
+                if _is_significant(score):
+                    (x, y, w, h) = bbox
+                    detections_to_display.append((x, y, w, h, score, category))
 
-        # Operations on the frame here
-        for (x, y, w, h) in displayed_detections:
-            cv.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 3)
+                    tracker = _create_single_tracker()
+                    multi_tracker.add(tracker, frame, bbox)
+        else:
+            tracking_result = _track_objects(multi_tracker, frame)
+            if not tracking_result:
+                tracker_failures += 1
+
+            detections_to_display.extend(tracking_result)
+
+        frame_at = time.time()
+        fps = int(1 / (frame_at - prev_frame_at))
+        prev_frame_at = frame_at
+
+        frame_count += 1
+        for detection in detections_to_display:
+            _display(frame, detection)
+
+        cv.putText(frame, f"{fps} FPS", (50, 50), font, 0.8, (0, 255, 0), 2)
 
         cv.imshow('frame', frame)
         if cv.waitKey(1) == ord('q'):
             break
     capture.release()
     cv.destroyAllWindows()
+    print(f"{tracker_failures} tracker failures")
+
+
+def _detect_objects(frame):
+    encoded = cv.imencode(".jpg", frame)[1]
+    frame_id = str(uuid.uuid4())
+    file = {'file': (f'{frame_id}.jpg', encoded.tobytes(), 'image/jpeg')}
+    data = {"id": f"{frame_id}"}
+    response = requests.post("http://127.0.0.1:8000/detection", files=file, data=data, timeout=5)
+    body = response.json()
+    print(f"Got: {body}")
+    return body['detections']
+
+
+def _to_bbox_score_category(detection):
+    bbox = detection['bbox']
+    score = detection['score']
+    category = detection['category']
+    return bbox, score, category
+
+
+def _is_significant(score):
+    return score >= 30
+
+
+def _track_objects(multi_tracker, frame):
+    result = []
+    ok, bboxes = multi_tracker.update(frame)
+    if ok:
+        for bbox in bboxes:
+            (x, y, w, h) = map(int, bbox)
+            result.append((x, y, w, h, None, None))
+    return result
+
+
+def _display(frame, detection):
+    (x, y, w, h, score, category) = detection
+    if score is not None and category is not None:
+        cv.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 3)
+        cv.putText(frame, f"{category} {score}%", (x, y - 10), font, 0.5, (255, 0, 0), 2)
+    else:
+        cv.rectangle(frame, (x, y), (x + w, y + h), (255, 180, 0), 2)
+
+
+def _create_multi_tracker():
+    return cv.legacy.MultiTracker_create()
+
+
+def _create_single_tracker():
+    return cv.legacy.TrackerKCF_create()
 
 
 if __name__ == '__main__':
--- edge-server/main.py
+++ edge-server/main.py
@@ -5,7 +5,18 @@ import mmcv
 config_file = '../detection-models/mobilenetv2_ssd.py'
 checkpoint_file = '../detection-models/work_dirs/mobilenetv2_ssd/latest.pth'
 model = init_detector(config_file, checkpoint_file, device='cpu')
-categories = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+categories = [
+    {"id": 0, "name": "pedestrian"},
+    {"id": 1, "name": "people"},
+    {"id": 2, "name": "bicycle"},
+    {"id": 3, "name": "car"},
+    {"id": 4, "name": "van"},
+    {"id": 5, "name": "truck"},
+    {"id": 6, "name": "tricycle"},
+    {"id": 7, "name": "awning-tricycle"},
+    {"id": 8, "name": "bus"},
+    {"id": 9, "name": "motor"}
+]
 
 app = FastAPI()
 
@@ -21,26 +32,31 @@ async def detect(file: UploadFile):
     image_id = file.filename
     image = mmcv.imfrombytes(frame)
     detections = inference_detector(model, image)
-    json = detections2json(detections)
+    json = _detections2json(detections)
     return {
         "image_id": image_id,
         "detections": json
     }
 
 
-def detections2json(detections):
+def _detections2json(detections):
     json = []
     for label in range(len(detections)):
         bboxes = detections[label]
         for i in range(bboxes.shape[0]):
             json.append({
-                "bbox": xyxy2xywh(bboxes[i]),
-                "score": float(bboxes[i][4]),
-                "category_id": categories[label]
+                "bbox": list(map(int, _xyxy2xywh(bboxes[i]))),
+                "score": int(bboxes[i][4] * 100),
+                "category": _category_name(label)
             })
     return json
 
 
-def xyxy2xywh(xyxy_bbox):
+def _xyxy2xywh(xyxy_bbox):
     bbox = xyxy_bbox.tolist()
     return [bbox[0], bbox[1], bbox[2] - bbox[0], bbox[3] - bbox[1]]
+
+
+def _category_name(category_id):
+    category = next(filter(lambda cat: cat["id"] == category_id, categories))
+    return category["name"]
